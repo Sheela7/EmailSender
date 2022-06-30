dba1fae820fb1186333aceadbdf775cec954655a
Sheela <sheela>
2022-06-30 11:28:24 +0545

    final code commited

--- routes/user.js
+++ routes/user.js
@@ -4,41 +4,34 @@ const nodemailer = require('nodemailer');
 
 
 
-// router.post('/', async (req, res) => {
-//     res.json=``
-//     }
-
+router.post('/', async (req, res) => {
+    const userEmail = req.body.email
+    console.log(userEmail)
+    
       // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: 'angila.pokhrel58@gmail.com', // generated ethereal user
-      pass: '@#password?!123!!!', // generated ethereal password
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASSWORD, // generated ethereal password
     },
     
   });
 
   // send mail with defined transport object
   let mailOptions = {
-    from: "angila.pokhrel58@gmail.com",
-    to: "sheela.pokhrel58@gmail.com", // list of receivers
+    from: process.env.EMAIL,
+    to: userEmail, // list of receivers
     subject: "sending email using node.js", // Subject line
     text: "hello this is email sent by me to u", // plain text body
   };
 
   transporter.sendMail(mailOptions, function(error, info) {
     if(error) {
-        console.log(error);
+       res.json("Error!!! Please enter email properly");
     } else{
-        console.log("email sent:" + info.response);
+      res.json(`Thank you for sending email ${userEmail}`) 
     }
   })
-
-  
-
- 
-
-
-
- 
+})
 module.exports = router;
